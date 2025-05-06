ffd32eee3757540e26556cda3d2e23e197fb47d9
dkvz <github@dkvz.eu>
2025-05-06 11:48:04 +0200

    comment options I need on my laptop or Windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,32 +5,35 @@ local wezterm = require("wezterm")
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
 function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
 end
 
 function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'Gruvbox Material (Gogh)'
-  else
-    return 'Catppuccin Latte (Gogh)'
-  end
+	if appearance:find("Dark") then
+		return "Gruvbox Material (Gogh)"
+	else
+		return "Catppuccin Latte (Gogh)"
+	end
 end
 
-
 return {
-  --color_scheme = 'Gruvbox Material (Gogh)',
-  color_scheme = 'Everforest Dark (Gogh)',
-  --color_scheme = 'TokyoNight (Gogh)',
-  --font = wezterm.font 'FiraCode Nerd Font',
-  font = wezterm.font 'CaskaydiaCove Nerd Font',
-  font_size = 13,
-  line_height = 1.0,
-  window_background_opacity = 0.97,
-  default_cursor_style = 'SteadyBar', 
-  term = "xterm-256color",
-  window_decorations = "TITLE | RESIZE",
-  hide_tab_bar_if_only_one_tab = true,
+	--color_scheme = 'Gruvbox Material (Gogh)',
+	color_scheme = "Everforest Dark (Gogh)",
+	--color_scheme = 'TokyoNight (Gogh)',
+	--font = wezterm.font 'FiraCode Nerd Font',
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	font_size = 13,
+	line_height = 1.0,
+	window_background_opacity = 0.97,
+	default_cursor_style = "SteadyBar",
+	term = "xterm-256color",
+	window_decorations = "TITLE | RESIZE",
+	hide_tab_bar_if_only_one_tab = true,
+	-- Needed on my laptop (might be better on Windows):
+	--freetype_load_target = "HorizontalLcd",
+	-- Uncomment on Windows:
+	--max_fps = 120,
 }
